a4b7691af43ca27250c94315e9f4b6e83166d140
Allan Kirk <allan@allans-mbp.home>
2024-09-09 19:29:04 -0400

    Method to count steps of  reducuction from 100 to 0

---
+++ src/Reduce.java
@@ -0,0 +1,17 @@
+public class Reduce {
+    public static void main(String[] args) {
+        int count = 0;
+        int i = 100;
+
+        while (i != 0) {
+            if(i%2==0){
+                i = i/2;
+            }
+            else {
+                i--;
+            }
+            count++;
+        }
+        System.out.println(count);
+    }
+}
